b4d17a056ff2747550c8fb277f59b836885c1eec
Sam <77967471+draumaz@users.noreply.github.com>
2022-01-20 07:37:45 -0800

    Add files via upload

--- src/header/gtool.h
+++ src/header/gtool.h
@@ -1,3 +1,3 @@
+const char* version();
 void game_sleep(int ms);
 void lbl_reader(const char* line, int stile);
-int user_input_int(int min, int max);
--- src/header/screen.h
+++ src/header/screen.h
@@ -1,7 +1,7 @@
-int board_again_screen();
+char in_prog_warn();
+char board_again_scren();
 void new_game_manager();
-void item_options_screen();
 void board_header_screen(int fake_options);
+void item_options_screen();
 void board_screen();
-int gip_warn();
 void splash_screen();
