c9550b60b8825d23f0c04c492765773203bda7f3
Sam <77967471+draumaz@users.noreply.github.com>
2022-01-27 17:50:21 -0800

    Add files via upload

--- src/header/screen.h
+++ src/header/screen.h
@@ -1,5 +1,7 @@
 void board_header(int x, int y);
 void board_header_update(int x, int y, int m);
 int entity_alive(int kind);
+void scr_poison(int x, int y);
+void scr_result(int x, int y);
 void scr_board();
 void scr_landing();
