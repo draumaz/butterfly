c7cb8db583e3e243c01dec293239aba2837f8f4a
Sam <77967471+draumaz@users.noreply.github.com>
2022-01-31 17:14:30 -0800

    Added centralized define file

---
+++ src/header/selections.h
@@ -0,0 +1,34 @@
+#ifndef CTRL
+#define CTRL(c) ((c) & 037)
+#endif
+
+#define NG_YES 9 // Y
+#define NG_NO 10
+
+#define RT_YES 15 // Y
+#define RT_NO 16
+
+#define BRD_FIGHT 7 // Y
+#define BRD_ITEMS 8
+#define BRD_SPARE 9
+#define BRD_EXIT 10
+
+#define LND_PLAY 8 // Y
+#define LND_RESET 9
+#define LND_CREDITS 10
+#define LND_EXIT 11
+
+#define HDR_KILL 25 // X
+#define HDR_DEATH 39
+#define HDR_SPARE 53
+#define HDR_RACE 7
+#define HDR_HP 20
+#define HDR_STR 30
+#define HDR_ITM_POTION 12 // Y
+#define HDR_ITM_SPEAR 13
+#define HDR_ITM_POISON 14
+
+#define ITM_POTION 12 // Y
+#define ITM_SPEAR 13
+#define ITM_POISON 14
+#define ITM_BACK 15
