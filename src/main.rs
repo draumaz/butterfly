31831d1985fb47084793f51b8959b8f010ef545f
Sam <77967471+draumaz@users.noreply.github.com>
2022-04-16 00:07:23 -0700

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+extern crate pancurses;
+
+mod master_display;
+mod disp_submods;
+mod batteries;
+mod nommes;
+
+use batteries::{screen_up, screen_down};
+use master_display::{splash_screen};
+use pancurses::{initscr, endwin};
+
+fn main() {
+	let screen = initscr();
+	screen_up(&screen);
+	splash_screen(&screen);
+	screen_down();
+	endwin();
+}
---
+++ src/nommes.rs
@@ -0,0 +1,7 @@
+pub static SAVE_DIR: &'static str = "save";
+
+pub static SAVE_NAME: &'static str = "save/data.txt";
+pub static SAVE_LENGTH: i32 = 20;
+
+pub static RECORD_NAME: &'static str = "save/record.txt";
+pub static RECORD_LENGTH: i32 = 5;
